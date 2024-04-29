8081ae298e8c8a1ee035d161be3785f3f61259b0
phbrandi <pedrohab1@al.insper.edu.br>
2024-04-29 20:32:29 -0300

    mapa mais bonito

--- Código.py
+++ Código.py
@@ -95,17 +95,17 @@ def f(mapa):
     for l, linha in enumerate (lista):
         for c, letra in enumerate (linha):
             if letra == 'N':
-                lista[l][c] = (u"\u001b[42mN\u001b[0m")
+                lista[l][c] = (u"\u001b[42m   \u001b[0m")
             if letra == 'X':
-                lista[l][c] = (u"\u001b[41mX\u001b[0m")
+                lista[l][c] = (u"\u001b[41m   \u001b[0m")
             if letra == 'A':
-                lista[l][c] = (u"\u001b[44mA\u001b[0m")
+                lista[l][c] = (u"\u001b[44m   \u001b[0m")
             if letra == ' ':
-                lista[l][c] = (u"\u001b[30m \u001b[0m") 
-    print ('  A B C D E F G H I J ')
+                lista[l][c] = (u"\u001b[30m   \u001b[0m") 
+    print ('   A   B   C   D   E   F   G   H   I   J ')
     for i in range (0, 10):
         print (i, lista[i][0], lista[i][1], lista[i][2], lista[i][3], lista[i][4], lista[i][5], lista[i][6], lista[i][7], lista[i][8], lista[i][9], i)
-    print ('  A B C D E F G H I J ')
+    print ('   A   B   C   D   E   F   G   H   I   J ')
     return ''
 
 
